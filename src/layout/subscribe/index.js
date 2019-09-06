243bb0ec98e58aabdd88561875e80545b84fc0ce
K Developer <kdev.software1110@gmail.com>
2019-09-06 15:17:28 -0400

    define subscribe section

--- src/layout/subscribe/index.js
+++ src/layout/subscribe/index.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 
 class Subscribe extends Component {
   render() {
-    return <div>Subscribe works!</div>;
+    return (
+      <div class="subscribe-banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2">
+              <h2>Subscribe to our newsletter</h2>
+
+              <form>
+                <input type="email" placeholder="E-mail address" />
+                <input
+                  class="button gradient-bg"
+                  type="submit"
+                  value="Subscribe"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
