de252d87381ea05a3f4f86968314ca662dbfa400
K Developer <kdev.software1110@gmail.com>
2019-09-08 23:04:48 -0400

    Updates

---
+++ precache-manifest.e3b7d69650f41ad3cd75d5a3af9e6ee0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed37cc3a0667bfe9a64afe0a38796156",
+    "url": "/k-medart-react/index.html"
+  },
+  {
+    "revision": "7c79c263bad4947f52d0",
+    "url": "/k-medart-react/static/css/2.5ff4851e.chunk.css"
+  },
+  {
+    "revision": "36af590a738b84a83198",
+    "url": "/k-medart-react/static/css/main.9222195a.chunk.css"
+  },
+  {
+    "revision": "7c79c263bad4947f52d0",
+    "url": "/k-medart-react/static/js/2.014db04b.chunk.js"
+  },
+  {
+    "revision": "36af590a738b84a83198",
+    "url": "/k-medart-react/static/js/main.67d5af2a.chunk.js"
+  },
+  {
+    "revision": "34e3791951f04222cc1a",
+    "url": "/k-medart-react/static/js/runtime~main.51f956e0.js"
+  },
+  {
+    "revision": "108675e88afaf9885651691be3946157",
+    "url": "/k-medart-react/static/media/about.108675e8.jpg"
+  },
+  {
+    "revision": "1dbb5bd7b4976d5d4d60ce06d7430b4d",
+    "url": "/k-medart-react/static/media/departments-bg.1dbb5bd7.jpg"
+  },
+  {
+    "revision": "f8762e9ba412227dbd13b3334874955a",
+    "url": "/k-medart-react/static/media/faq-bg.f8762e9b.jpg"
+  },
+  {
+    "revision": "323b1514c4e3f2c88ebc87ee5e198f81",
+    "url": "/k-medart-react/static/media/footer-bg.323b1514.png"
+  },
+  {
+    "revision": "ec6a44463748b415868ac8f8013244a3",
+    "url": "/k-medart-react/static/media/hero.ec6a4446.jpg"
+  },
+  {
+    "revision": "e271e7b0308e72506456e3e521c19af9",
+    "url": "/k-medart-react/static/media/news-1.e271e7b0.png"
+  },
+  {
+    "revision": "678b166977d6dfe03765e664fd2f1ce0",
+    "url": "/k-medart-react/static/media/news-2.678b1669.png"
+  },
+  {
+    "revision": "26bebb9a9541f2f0cc863929f37d0cec",
+    "url": "/k-medart-react/static/media/news-3.26bebb9a.png"
+  },
+  {
+    "revision": "8fa68a4969ca560bd80b989689f361e4",
+    "url": "/k-medart-react/static/media/news-4.8fa68a49.png"
+  },
+  {
+    "revision": "ad54737516c699e7fdc54f7a9a222cdf",
+    "url": "/k-medart-react/static/media/news-5.ad547375.png"
+  },
+  {
+    "revision": "9c90475dae34df884d211f759ad3fff9",
+    "url": "/k-medart-react/static/media/news-6.9c90475d.png"
+  },
+  {
+    "revision": "3f27345f47ffc6975b57a14c2b7c29f8",
+    "url": "/k-medart-react/static/media/service-tab-img.3f27345f.png"
+  },
+  {
+    "revision": "d9bc19793f4088679a43d7ddcf6ca95b",
+    "url": "/k-medart-react/static/media/subscribe-bg.d9bc1979.png"
+  },
+  {
+    "revision": "1cb933064e65723131c4bda44ca77f7d",
+    "url": "/k-medart-react/static/media/team-1.1cb93306.jpg"
+  },
+  {
+    "revision": "5a87c75dc95b88b55b90673cd4d194b9",
+    "url": "/k-medart-react/static/media/team-2.5a87c75d.jpg"
+  },
+  {
+    "revision": "792e7e2c5b3003b7cf7ca6619a4febea",
+    "url": "/k-medart-react/static/media/team-3.792e7e2c.jpg"
+  },
+  {
+    "revision": "e11669a2f5bfd2bb69b6988e03759abb",
+    "url": "/k-medart-react/static/media/team-4.e11669a2.jpg"
+  },
+  {
+    "revision": "79b28b680a4ed1a62592d2f2a75b1853",
+    "url": "/k-medart-react/static/media/testimonial-bg.79b28b68.jpg"
+  },
+  {
+    "revision": "d696d405d133acb71c32e05e2d24ba67",
+    "url": "/k-medart-react/static/media/testimonial-content-bg.d696d405.png"
+  }
+]);
