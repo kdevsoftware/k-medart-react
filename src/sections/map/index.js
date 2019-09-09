495e158353fa54d71552cbb7982fcf728d933ae1
K Developer <kdev.software1110@gmail.com>
2019-09-08 22:46:49 -0400

    google map section ui

--- src/sections/map/index.js
+++ src/sections/map/index.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 
 class Map extends Component {
   render() {
-    return <div>Map works!</div>;
+    return (
+      <div>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="contact-page-map">
+                <iframe
+                  id="gmap_canvas"
+                  src="https://maps.google.com/maps?q=university of san francisco&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
